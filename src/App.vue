26ac5d8f000d6c6f5f9872f54c54a50449d5a0ae
liho <tanlh-wa16@student.tarc.edu.my>
2019-06-29 15:35:36 +0800

    liho commit

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,23 @@
   <div id="app">
     <Header />
     <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from "./components/layout/Header";
-
-import "@/assets/js/jquery.min.js";
-import "bootstrap";
-import "@/assets/js/owl.carousel.min.js";
-import "@/assets/js/custom.js";
+import Header from "./components/layout/Header.vue";
+import Footer from "./components/layout/Footer.vue";
 
 export default {
   name: "app",
   components: {
-    Header
+    Header,
+    Footer
   }
 };
 </script>
 
-<style lang="scss">
-@import "./assets/css/owl.carousel.min.css";
-@import "./assets/css/styles.css";
-@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
-@import url("https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900");
-@import "./assets/css/bootstrap.min.css";
+<style>
 
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
-  base: process.env.BASE_URL,
+  // mode: "history",
+  // base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
@@ -14,19 +15,9 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    },
-    {
       path: "/login",
       name: "login",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Login.vue")
+      component: Login
     }
   ]
 });
