90a07ea58faa695f0513c1f06ae13376f3ee0f3d
jtmycraft <30696041+jtmycraft@users.noreply.github.com>
2019-07-11 09:45:40 +0800

    Updated Search Book

--- src/views/books/SearchBook.vue
+++ src/views/books/SearchBook.vue
@@ -8,28 +8,76 @@
       </div>
     </div>
 
-    <div class="search-book centre">
+    <b-container fluid>
+    <!-- User Interface controls -->
+    <b-row>
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Filter" class="mb-0">
+          <b-input-group>
+            <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
 
-      <input
-        class="form-control"
-        type="text"
-        name="keyword"
-        v-model="keyword"
-        placeholder="Keyword"
-        style="display: inline"
-        v-validate="'required'"
-        required
-      />
-      <br/>
-      <input
-        class="btn"
-        type="submit"
-        @click="searchBook"
-        value="Search"
-      />
-    </div>
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Sort" class="mb-0">
+          <b-input-group>
+            <b-form-select v-model="sortBy" :options="sortOptions">
+              <option slot="first" :value="null">-- none --</option>
+            </b-form-select>
+            <b-form-select v-model="sortDesc" :disabled="!sortBy" slot="append">
+              <option :value="false">Asc</option> <option :value="true">Desc</option>
+            </b-form-select>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
 
-    
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Sort direction" class="mb-0">
+          <b-form-select v-model="sortDirection">
+            <option value="asc">Asc</option>
+            <option value="desc">Desc</option>
+            <option value="last">Last</option>
+          </b-form-select>
+        </b-form-group>
+      </b-col>
+
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Per page" class="mb-0">
+          <b-form-select v-model="perPage" :options="pageOptions"></b-form-select>
+        </b-form-group>
+      </b-col>
+    </b-row>
+
+    <!-- Main table element -->
+    <b-table
+      show-empty
+      stacked="md"
+      :items="items"
+      :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage"
+      :filter="filter"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      :sort-direction="sortDirection"
+      @filtered="onFiltered"
+    >
+    </b-table>
+
+    <b-row class="row d-flex justify-content-center">
+        <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="my-0"
+        ></b-pagination>
+    </b-row>
+  </b-container>
+
   </div>
 </template>
 
@@ -38,31 +86,89 @@ import db from "./../../components/firestoreInit";
 import Vue from "vue";
 import VeeValidate from "vee-validate";
 import { firestorePlugin } from "vuefire";
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 Vue.use(firestorePlugin);
 Vue.use(VeeValidate);
 
+// This imports all the layout components such as <b-container>, <b-row>, <b-col>:
+import { LayoutPlugin } from 'bootstrap-vue'
+Vue.use(LayoutPlugin)
+
+// This imports <b-modal> as well as the v-b-modal directive as a plugin:
+import { ModalPlugin } from 'bootstrap-vue'
+Vue.use(ModalPlugin)
+
+// This imports <b-card> along with all the <b-card-*> sub-components as a plugin:
+import { CardPlugin } from 'bootstrap-vue'
+Vue.use(CardPlugin)
+
+// This imports directive v-b-scrollspy as a plugin:
+import { VBScrollspyPlugin } from 'bootstrap-vue'
+Vue.use(VBScrollspyPlugin)
+
+// This imports the dropdown and table plugins
+import { DropdownPlugin, TablePlugin } from 'bootstrap-vue'
+Vue.use(DropdownPlugin)
+Vue.use(TablePlugin)
+
 export default {
   name: "search-book",
-  data() {
+  data () {
     return {
-      books: []
-    };
+      items: [],
+      fields: [
+        { key: 'book_id', label: 'Book ID', sortable: true },
+        { key: 'title', label: 'Title', sortable: true },
+        { key: 'author', label: 'Author', sortable: true },
+        { key: 'publisher', label: 'Publisher', sortable: true },
+        { key: 'year', label: 'Year', sortable: true },
+        { key: 'quantity', label: 'Quantity', sortable: true },
+      ],
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 5,
+      pageOptions: [5, 10, 15],
+      sortBy: null,
+      sortDesc: false,
+      sortDirection: 'asc',
+      filter: null
+    }
   },
-  // vuefire library
-  firestore: {
-    // get books that are available from firebase
-    books: db.collection("books").orderBy("book_id")
+  firestore () {
+    return {
+      items: db.collection("books").orderBy("book_id")
+    }
+  },
+  computed: {
+    sortOptions() {
+      // Create an options list from our fields
+      return this.fields
+        .filter(f => f.sortable)
+        .map(f => {
+          return { text: f.label, value: f.key }
+        })
+    },
+    rows() {
+      return this.items.length
+    }
+  },
+  mounted() {
+    // Set the initial number of items
+    this.totalRows = this.items.length
   },
   methods: {
-    searchBook() {
-      alert("PRINT")
-      console.log(this.books)
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
     }
   }
 };
 </script>
 
- <style scoped>
+<style scoped>
 div.centre {
   text-align: center;
   width: 100%;
@@ -105,4 +211,19 @@ p a {
   text-decoration: underline;
   cursor: pointer;
 }
+
+table {
+  margin-left: 10%;
+  margin-right: 10%;
+  border-collapse: collapse;
+  width: 80%;
+}
+
+th, td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+
+tr:hover {background-color:#f5f5f5;}
 </style>
